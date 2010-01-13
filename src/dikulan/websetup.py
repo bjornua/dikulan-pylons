9167b0cc7d2dbb7c09de9d9aebb72e929be0b060
Bjørn Uhre Arnholtz <rugkiks@gmail.com>
2010-01-13 13:59:52 +0100

    Tilføjede database-oprettelse

--- src/dikulan/websetup.py
+++ src/dikulan/websetup.py
@@ -1,5 +1,5 @@
 """Setup the dikulan application"""
-import logging
+import logging, os, sqlite3
 
 from dikulan.config.environment import load_environment
 
@@ -8,3 +8,21 @@ log = logging.getLogger(__name__)
 def setup_app(command, conf, vars):
     """Place any commands to setup dikulan here"""
     load_environment(conf.global_conf, conf.local_conf)
+    
+    if not os.path.exists("data/database.sqlite"):
+        log.debug("No database-file present, create new database-file.")
+        db = sqlite3.connect("data/database.sqlite")
+        c = db.cursor()
+        
+        log.debug("Creating reserved_seats table.")
+        c.execute("""
+            CREATE TABLE "reserved_seats" (
+                "id" INTEGER PRIMARY KEY
+            );
+        """)
+        log.debug("Done creating database.")
+        
+    else:
+        log.debug("It did. Will not touch it then.")
+    
+    log.debug("Done setting up application.")
